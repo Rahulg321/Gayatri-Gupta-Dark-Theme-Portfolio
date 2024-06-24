6f39553931cdbfc691d3263410e28e10de93b499
Rahul Gupta <rg5353070@gmail.com>
2024-06-24 11:28:09 +0530

    removed title from content body

--- components/ContentBody.tsx
+++ components/ContentBody.tsx
@@ -1,6 +1,6 @@
 import { components } from "@/slices";
 import { Content } from "@prismicio/client";
-import { SliceZone } from "@prismicio/react";
+import { PrismicRichText, SliceZone } from "@prismicio/react";
 import React from "react";
 
 const ContentBody = ({
@@ -10,7 +10,6 @@ const ContentBody = ({
 }) => {
   return (
     <article className="block-space narrow-container">
-      <h2 className="mb-8">{page.data.title}</h2>
       <SliceZone slices={page.data.slices} components={components} />
     </article>
   );
