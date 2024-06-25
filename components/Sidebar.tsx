a0117ad14632b05a1de9fa58eb8fd7a28ea0bb8b
Rahul Gupta <rg5353070@gmail.com>
2024-06-25 18:11:38 +0530

    Update Sidebar.tsx and added links to linkedin and upwork

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,8 +32,8 @@ const DesktopSidebar = () => {
         <h4 className="mt-8">Connect</h4>
         <div className=" mt-2 space-y-2">
           <NavLink label="Contact" href="/contact" icon={<FileText />} />
-          <NavLinkArrow label="LinkedIn" href="" icon={<Linkedin />} />
-          <NavLinkArrow label="Upwork" href="" icon={<Briefcase />} />
+          <NavLinkArrow label="LinkedIn" href="https://www.linkedin.com/in/gayatri-gupta-377060233/" icon={<Linkedin />} />
+          <NavLinkArrow label="Upwork" href="https://www.upwork.com/freelancers/~01473de59e63e94a6a?referrer_url_path=%2Fnx%2Fsearch%2Ftalent%2Fdetails%2F~01473de59e63e94a6a%2Fprofile" icon={<Briefcase />} />
         </div>
         <div className="mt-4">
           <ModeToggle />
@@ -80,6 +80,7 @@ export function NavLinkArrow({
       href={href}
       className="text-muted-foreground transition-all
       duration-200 ease-in-out hover:text-foreground flex items-center justify-between p-2 rounded-md"
+      target="_blank"
     >
       <div className="flex items-center gap-4">
         <div className="h-[1.2rem] w-[1.2rem]">{icon}</div>
