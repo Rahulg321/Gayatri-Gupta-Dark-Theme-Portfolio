0e16967dbb4d03e4d1fc112c089f281b65d0c4f0
Rahul Gupta <rg5353070@gmail.com>
2024-07-17 22:44:00 +0530

    updated the fetch call for no store

--- prismicio.ts
+++ prismicio.ts
@@ -40,7 +40,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
       process.env.NODE_ENV === "production"
         ? {
             next: { tags: ["prismic"], revalidate: 86400 },
-            cache: "no-store",
+            cache: "no-cache",
           }
         : { next: { revalidate: 5 } },
     ...config,
